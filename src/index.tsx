31956e9fbd1ab680ae9900b4574b5571913a16a8
tozzi <pedrogbtozzi@gmail.com>
2021-07-26 00:58:26 -0300

    Creating home page, button component and global style (with sass)

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,14 @@ import App from './App';
 
 import './services/firebase.ts';
 
+import './styles/global.scss';
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 ReactDOM.render(
     <React.StrictMode>
-    <App />
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
-    );
-    
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleImg from '../assets/images/google-icon.svg'
+import appleImg from '../assets/images/apple-logo.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+export function Home () {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Illustration describing a virtual question or doubt"/>
+                <strong>Make your own Q&amp;A live room</strong>
+                <p>Answer questions from your live audience</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="LetMeAsk's logo"/>
+                    <p className="separator">Create my own room with</p>
+                    <button className="create-room-google">
+                        <img src={googleImg} alt="Google's logo"/>
+                        Google
+                    </button>
+                    <button className="create-room-apple">
+                        <img src={appleImg} alt="Apple's logo"/>
+                        Apple
+                    </button>
+                    <div className="separator">or join an existent room</div>
+                    <form>
+                        <input 
+                            type="text"
+                            placeholder="Type room's code"
+                        />
+                        <Button type="submit">
+                            Join room
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
